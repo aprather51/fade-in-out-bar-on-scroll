36b8760d7cec07f0cd362b2c7cbc77e38e3cc1d7
Arthur Prather <apratherjr@gmail.com>
2019-04-18 13:18:22 -0500

    create build/ for gh-page

---
+++ build/precache-manifest.876e322235bc7c17c311e821ed383a2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "34f24bc58f3fdf4fda0d",
+    "url": "/Header-Fade-Hide-Show/static/js/runtime~main.34f24bc5.js"
+  },
+  {
+    "revision": "ebe88adad2238c56c9a5",
+    "url": "/Header-Fade-Hide-Show/static/js/main.ebe88ada.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/Header-Fade-Hide-Show/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "ebe88adad2238c56c9a5",
+    "url": "/Header-Fade-Hide-Show/static/css/main.23c991ae.chunk.css"
+  },
+  {
+    "revision": "beb992032eab321c0c28a0fd850f0ad6",
+    "url": "/Header-Fade-Hide-Show/index.html"
+  }
+];
